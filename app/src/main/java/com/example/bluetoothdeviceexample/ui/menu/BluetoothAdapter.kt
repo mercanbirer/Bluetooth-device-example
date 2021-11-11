9ddd3307d0fb2d1fca0d642ed344ca64cce3edf7
Mercan Birer <mercan@redline.com.tr>
2021-11-11 14:08:50 +0300

    Added ble list and selection list item

--- app/src/main/java/com/example/bluetoothdeviceexample/ui/menu/BluetoothAdapter.kt
+++ app/src/main/java/com/example/bluetoothdeviceexample/ui/menu/BluetoothAdapter.kt
@@ -9,7 +9,7 @@ import com.example.bluetoothdeviceexample.base.BaseAdapter
 import com.example.bluetoothdeviceexample.databinding.ItemBluetoothBinding
 
 class BluetoothAdapter(
-    items: ArrayList<BluetoothDevice>,
+    var items: ArrayList<BluetoothDevice>,
     private val ctx: Context,
 ) : BaseAdapter<BluetoothDevice>(ctx, items) {
     private val applicationInfos: List<BluetoothDevice> = items
@@ -26,6 +26,16 @@ class BluetoothAdapter(
 
         val model: BluetoothDevice = applicationInfos[position]
 
+        if (model.name != null){
+            binding.tvApp.text = model.name
+        }else{
+            binding.tvApp.text = model.address
+        }
         return binding.root
+    }
+
+    fun updateList(newItems: java.util.ArrayList<BluetoothDevice>) {
+        items = newItems
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/bluetoothdeviceexample/ui/menu/BluetoothFragment.kt
+++ app/src/main/java/com/example/bluetoothdeviceexample/ui/menu/BluetoothFragment.kt
@@ -1,20 +1,37 @@
 package com.example.bluetoothdeviceexample.ui.menu
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.content.*
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.bluetoothdeviceexample.R
 import com.example.bluetoothdeviceexample.base.BaseFragment
 import com.example.bluetoothdeviceexample.databinding.FragmentBluetoothBinding
 import com.github.ajalt.timberkt.e
 
+
 class BluetoothFragment : BaseFragment<FragmentBluetoothBinding>(R.layout.fragment_bluetooth) {
     private var deviceItemList = ArrayList<BluetoothDevice>()
-    var bTAdapter: BluetoothAdapter? = null
+    var bTAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    var blAdapter: com.example.bluetoothdeviceexample.ui.menu.BluetoothAdapter? = null
+    private val filters = IntentFilter(BluetoothDevice.ACTION_FOUND)
+    private var bReciever: BroadcastReceiver? = null
+    private val mDeviceList: ArrayList<String> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        requireActivity().registerReceiver(bReciever, filter)
         val pairedDevices = bTAdapter.bondedDevices
         if (pairedDevices.size > 0) {
             e { "geldi $pairedDevices" }
@@ -27,5 +44,60 @@ class BluetoothFragment : BaseFragment<FragmentBluetoothBinding>(R.layout.fragme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        blAdapter = BluetoothAdapter(deviceItemList, requireContext())
+        binding.lvBle.adapter = blAdapter
+
+        bReciever = object : BroadcastReceiver() {
+            @SuppressLint("LogNotTimber")
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+                if (BluetoothDevice.ACTION_FOUND == action) {
+
+                    Log.d("DEVICELIST", "Bluetooth device found\n")
+                    val device =
+                        intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                    device?.let {
+                        deviceItemList.add(device)
+                        Log.e("dekmlf", deviceItemList.toString())
+                    }
+                    blAdapter!!.notifyDataSetChanged()
+                }
+            }
+        }
+
+
+        binding.lvBle.setOnItemClickListener { _, _, position, _ ->
+            bTAdapter.cancelDiscovery()
+            val value: String = deviceItemList.toList()[position].address
+            Log.e("blsjddknwfşml", deviceItemList.toList()[position].toString())
+            val device: BluetoothDevice = bTAdapter.getRemoteDevice(value)
+            blAdapter!!.notifyDataSetChanged()
+            if (device.bondState == BluetoothDevice.BOND_BONDED) {
+                pair(device)
+            } else {
+                Toast.makeText(requireContext(), "not connected", 500).show()
+            }
+        }
     }
+
+    private fun pair(device: BluetoothDevice) {
+        device.createBond()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().registerReceiver(bReciever, filters)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            requireActivity().unregisterReceiver(bReciever)
+        } catch (exception: ReceiverCallNotAllowedException) {
+        }
+    }
+
 }
